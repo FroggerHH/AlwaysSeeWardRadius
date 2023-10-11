4fc3a2ed224a2a630b32dab7bfbc843e112d3a09
Vyacheslav <slava.shilin07@mail.ru>
2023-10-11 14:04:05 +0300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace AlwaysSeeWardRadius;
+
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string
+        ModName = "AlwaysSeeWardRadius",
+        ModVersion = "1.1.0",
+        ModGUID = $"com.{ModAuthor}.{ModName}",
+        ModAuthor = "Frogger";
+
+    public static ConfigEntry<bool> enabledConfig;
+
+    private void Awake()
+    {
+        CreateMod(this, ModName, ModAuthor, ModVersion);
+        enabledConfig = config("General", "Enabled", true, "");
+    }
+}
